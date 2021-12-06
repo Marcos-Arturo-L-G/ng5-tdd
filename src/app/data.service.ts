5f79c450507773efb0fb54b6a4b35c765931a916
Marcos-Arturo-L-G <marcosarturol.g@gmail.com>
2021-12-06 09:21:06 -0600

    fallo de api copiando todo el archivo del profe

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,19 +1,78 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+//import { ResponseApi } from '../../models/responseapi';
+import { GoalsApi } from './goalsApi';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject } from 'rxjs/';
 @Injectable({
   providedIn: 'root'
 })
-
 export class DataService {
 
-  private goals = new BehaviorSubject<any> (['the inital goal','another silly life goal']);
-  goal = this.goals.asObservable();
-	//apiurl="apiurl"
+  private goals = new BehaviorSubject<any>(['La meta inicial', 'Otra meta :P']);
+  goal=  this.goals.asObservable();
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  changeGoal(goal: any){
-    this.goals.next(goal);
+  changeGoal(goal){
+    this.goals.next(goal)
   }
+
+  //apiURL = 'http://localhost:8080';
+  apiURL = 'http://34.125.7.41:8112/api';
+
+  // Http Options
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Origin':'*',
+      'Access-Control-Allow-Methods':'GET' 
+      })
+  }  
+
+  
+
+
+  getGoals(): Observable<GoalsApi> {
+    return this.http.get<GoalsApi>(this.apiURL + '/users', this.httpOptions)
+    .pipe(
+      retry(1),
+      catchError(this.handleError)
+    )
+  }   
+
+  newGoal(payload): Observable<GoalsApi> {
+    return this.http.post<GoalsApi>(this.apiURL + '/users', JSON.stringify(payload), this.httpOptions)
+    .pipe(
+      retry(1),
+      catchError(this.handleError)
+    )
+  }
+
+/*
+  postParks(parksApi): Observable<ParksApi> {
+    //console.log("print post: " + this.apiURL + '/parques', JSON.stringify(parksApi), this.httpOptions)
+    return this.http.post<ParksApi>(this.apiURL + '/parques', JSON.stringify(parksApi), this.httpOptions)
+    .pipe(
+      retry(1),
+      catchError(this.handleError)
+    )
+  }*/
+  
+  // Error handling 
+  handleError(error) {
+    let errorMessage = '';
+    if(error.error instanceof ErrorEvent) {
+      // Get client-side error
+      errorMessage = error.error.message;
+    } else {
+      // Get server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    window.alert(errorMessage);
+    return throwError(errorMessage);
+ }
 
 }
---
+++ src/app/goalsApi.ts
@@ -0,0 +1,5 @@
+export class GoalsApi {
+    name: string;
+    itemID: string;
+    numericalID: string;
+  }
