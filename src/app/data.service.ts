8c39a9e84c4c08de42ec8497ea9b4dca92473404
Marcos-Arturo-L-G <marcosarturol.g@gmail.com>
2021-12-07 17:15:23 -0600

    semi_final

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,3 +1,8 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+//import { ResponseApi } from '../../models/responseapi';
+import { GoalsApi } from './goalsApi';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 @Injectable({
@@ -8,12 +13,43 @@ export class DataService {
 
   private goals = new BehaviorSubject<any> (['the inital goal','another silly life goal']);
   goal = this.goals.asObservable();
-	//apiurl="apiurl"
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   changeGoal(goal: any){
     this.goals.next(goal);
   }
+
+  apiURL = 'http://34.125.7.41:8112/api';
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Origin':'*',
+      'Access-Control-Allow-Methods':'GET' 
+      })
+  }
+
+  getGoals(): Observable<GoalsApi>{
+    return this.http.get<GoalsApi>(this.apiURL + '/minecraft', this.httpOptions)
+    .pipe(
+      retry(1),
+      catchError(this.handleError)
+    )
+  }
+
+  handleError(error: any) {
+    let errorMessage = '';
+    if(error.error instanceof ErrorEvent) {
+      // Get client-side error
+      errorMessage = error.error.message;
+    } else {
+      // Get server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    window.alert(errorMessage);
+    return throwError(errorMessage);
+ }
 
 }
